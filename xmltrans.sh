bf91572796fd74335bfbf2a23d762012befc72a8
shiki <james602152002@hotmail.com>
2023-04-11 15:21:30 +0800

    update

---
+++ xmltrans.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+mkdir ~/Documents/localization
+mkdir ~/Documents/localization_merged
+cp ~/Downloads/localization.zip ~/Documents/localization
+unzip ~/Documents/localization/localization.zip -d  ~/Documents/localization/
+./mergeLoc.py
+rm ~/Downloads/localization.zip
+cd ~/Documents/localization_merged
+zip -r localization.zip Saury*
+cd ~/scripts/XMLTrans
+cp ~/Documents/localization_merged/localization.zip ~/Downloads
+rm -r ~/Documents/localization_merged
+rm -r ~/Documents/localization
+mkdir ~/Documents/localization
+cp ~/Downloads/localization.zip ~/Documents/localization
+unzip ~/Documents/localization/localization.zip -d  ~/Documents/localization/
+./xmltrans.py
+rm -r ~/Documents/localization
+rm ~/Downloads/localization.zip
